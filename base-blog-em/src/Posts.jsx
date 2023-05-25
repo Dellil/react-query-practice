1de5d045e2c3cfba08c3cc5951751c1c39021ba2
Chanhee Jang <aksaksdm@naver.com>
2023-05-26 00:12:51 +0900

    learn: useQuery 맛 보기

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { PostDetail } from "./PostDetail";
+import { useQuery } from "react-query";
 const maxPostPage = 10;
 
 async function fetchPosts() {
@@ -14,8 +15,16 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {
+    staleTime: 2000,
+  });
+  if (isLoading) return <h3>Loading...</h3>
+  if (isError) return (
+    <>
+      <h3>Oops, something went wrong</h3>
+      {error.toString()}
+    </>
+  )
 
   return (
     <>
